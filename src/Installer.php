e95e3c215e61d52fd1f7b907ff977c61f0823c3f
Olivier Gorzalka <olivier@amphibee.fr>
2022-06-28 13:38:32 +0200

    Update gist

--- src/Installer.php
+++ src/Installer.php
@@ -4,7 +4,7 @@ namespace AmphiBee\LaravelPintPreCommit;
 
 class Installer
 {
-    const FETCH_URL = "https://gist.githubusercontent.com/ogorzalka/2d355e35c9308cd274ce3232de4cd19c/raw/7a73070c317fcb5e6f58d040d1afa909b5df2b40/pre-commit.sh";
+    const FETCH_URL = "https://gist.githubusercontent.com/ogorzalka/2d355e35c9308cd274ce3232de4cd19c/raw/570166574382b71c59e8803d7343143979d13c61/pre-commit.sh";
 
     public function install()
     {
