15fcc45586e30057bdb07719aad37cd3fa405e31
silentpatrik <77385235+silentpatrik@users.noreply.github.com>
2023-06-16 20:06:57 +0200

    Update Installer.php
    wip
    fetchUrl from environment variables  but fallback to default.
    added url whitelisting and validator rule for fetchurl

--- src/Installer.php
+++ src/Installer.php
@@ -5,10 +5,48 @@ namespace AmphiBee\LaravelPintPreCommit;
 class Installer
 {
     const FETCH_URL = "https://gist.githubusercontent.com/ogorzalka/2d355e35c9308cd274ce3232de4cd19c/raw/570166574382b71c59e8803d7343143979d13c61/pre-commit.sh";
+    protected mixed $fetchUrl; // mixed so we can have an array in future when multiple pre-commit scripts should be runned.
 
+    public function setFetchUrl()
+    {
+        $fetchUrl = getenv('PRE_COMMIT_FETCH_URL')!== false ? getenv('LARAVEL_PRE_COMMIT_FETCH_URL') : Installer::FETCH_URL;
+    }
+    
+    public function validateFetchUrl($url) : bool 
+    {
+       // i will move this to its own file when not coding in github, text editor
+        class FetchUrlRule implements Illuminate\Contracts\Validation\Rule
+        {
+             protected mixed $urlWhiteList = ['https://gist.githubusercontent.com/'];
+            public function __construct(mixed $urlWhiteList=null)
+            {
+                if(!is_null($urlWhiteList))
+                {
+                    $this->urlWhiteList = $urlWhiteList;
+                }
+            }
+        
+            public function passes( $attribute, $value ) 
+            {
+                $whiteListed=false;
+                 foreach($this->urlWhiteList as $url) {
+                     if(strpos($url, $value)!==false){
+                         $whiteListed=true;
+                         break;
+                     }
+                 }   
+    	        return ($whiteListed && filter_var($value, FILTER_VALIDATE_URL)) ? true : false;
+    	    }
+
+    	    public function message() {
+    		    return ':attribute must be a valid url and the url must be whitelisted.';
+            }
+        }
+        $validator = new FetchUrlRule();
+    }
+    
     public function install()
     {
-
         if (!$this->checkGitHookDir())
         {
             throw new \Exception("Not a git repository");
